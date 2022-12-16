c3773231bebd46bbd44c629b9de74b71a7cdb5dc
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-16 22:45:13 +0300

    v21

--- blogapp/src/components/UserPost.jsx
+++ blogapp/src/components/UserPost.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const UserPost = () => {
-  return (
-    <div>userPost</div>
-  )
-}
+  const { posts } = useSelector((state) => state.postsSlice
+);
+  const { userInfo:{uid} } = useSelector((state) => state.loginInfos);
+  console.log(posts);
+  console.log(uid);
+  return <div>
+    {posts.map((item) => {
+return(<>{item.uid === uid &&<p>{`${item.id}, ${item.author}`}</p>}</>)})}
+  </div>;
+};
 
-export default UserPost
+export default UserPost;
